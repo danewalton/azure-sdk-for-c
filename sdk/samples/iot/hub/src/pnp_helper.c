98eda11ac03e71beebdae8a321023264f5d86b13
Dane Walton <dane.walton@microsoft.com>
2020-07-16 15:53:54 -0700

    add more pnp helper functions

--- sdk/samples/iot/hub/src/pnp_helper.c
+++ sdk/samples/iot/hub/src/pnp_helper.c
@@ -4,48 +4,152 @@
 #include <azure/core/az_json.h>
 #include <azure/core/az_result.h>
 #include <azure/core/az_span.h>
+#include <azure/iot/az_iot_hub_client.h>
 
 #include "pnp_helper.h"
 
+#define JSON_DOUBLE_DIGITS 2
+
+static az_span component_telemetry_prop_span = AZ_SPAN_LITERAL_FROM_STR("$.sub");
+static const az_span desired_temp_response_value_name = AZ_SPAN_LITERAL_FROM_STR("value");
+static const az_span desired_temp_ack_code_name = AZ_SPAN_LITERAL_FROM_STR("ac");
+static const az_span desired_temp_ack_version_name = AZ_SPAN_LITERAL_FROM_STR("av");
+static const az_span desired_temp_ack_description_name = AZ_SPAN_LITERAL_FROM_STR("ad");
 static char component_specifier_name[] = "__t";
 static char component_specifier_value[] = "c";
 static char command_separator[] = "*";
 
-static az_result build_component_reported_property(
-    az_span json_buffer,
+static void strip_quotes_from_span(az_span input_span, az_span* out_span)
+{
+  *out_span = az_span_slice(input_span, 1, az_span_size(input_span) - 1);
+}
+
+static az_result build_reported_property_with_status(
+    az_json_writer* json_writer,
+    az_span property_name,
+    az_span property_val,
+    int32_t ack_code_value,
+    int32_t ack_version_value,
+    az_span ack_description_value)
+{
+  AZ_RETURN_IF_FAILED(az_json_writer_append_begin_object(json_writer));
+  AZ_RETURN_IF_FAILED(az_json_writer_append_property_name(json_writer, property_name));
+  AZ_RETURN_IF_FAILED(az_json_writer_append_begin_object(json_writer));
+  AZ_RETURN_IF_FAILED(
+      az_json_writer_append_property_name(json_writer, desired_temp_response_value_name));
+
+  if (*(char*)az_span_ptr(property_val) == '"')
+  {
+    az_span stripped_val;
+    strip_quotes_from_span(property_val, &stripped_val);
+    AZ_RETURN_IF_FAILED(az_json_writer_append_string(json_writer, stripped_val));
+  }
+  else
+  {
+    double value_as_double;
+    AZ_RETURN_IF_FAILED(az_span_atod(property_val, &value_as_double));
+    AZ_RETURN_IF_FAILED(
+        az_json_writer_append_double(json_writer, value_as_double, JSON_DOUBLE_DIGITS));
+  }
+
+  AZ_RETURN_IF_FAILED(az_json_writer_append_property_name(json_writer, desired_temp_ack_code_name));
+  AZ_RETURN_IF_FAILED(az_json_writer_append_int32(json_writer, ack_code_value));
+  AZ_RETURN_IF_FAILED(
+      az_json_writer_append_property_name(json_writer, desired_temp_ack_version_name));
+  AZ_RETURN_IF_FAILED(az_json_writer_append_int32(json_writer, ack_version_value));
+
+  if (az_span_ptr(ack_description_value) != NULL)
+  {
+    AZ_RETURN_IF_FAILED(
+        az_json_writer_append_property_name(json_writer, desired_temp_ack_description_name));
+    AZ_RETURN_IF_FAILED(az_json_writer_append_string(json_writer, ack_description_value));
+  }
+
+  AZ_RETURN_IF_FAILED(az_json_writer_append_end_object(json_writer));
+  AZ_RETURN_IF_FAILED(az_json_writer_append_end_object(json_writer));
+
+  return AZ_OK;
+}
+
+static az_result build_reported_property(
+    az_json_writer* json_writer,
     az_span component,
     az_span name,
     az_span value,
     az_span* out_span)
 {
-  az_json_writer json_writer;
-  AZ_RETURN_IF_FAILED(
-      az_json_writer_init(&json_writer, json_buffer, NULL));
-  AZ_RETURN_IF_FAILED(az_json_writer_append_begin_object(&json_writer));
-  AZ_RETURN_IF_FAILED(az_json_writer_append_property_name(&json_writer, component));
-  AZ_RETURN_IF_FAILED(az_json_writer_append_begin_object(&json_writer));
-  AZ_RETURN_IF_FAILED(az_json_writer_append_property_name(
-      &json_writer, AZ_SPAN_FROM_BUFFER(component_specifier_name)));
-  AZ_RETURN_IF_FAILED(
-      az_json_writer_append_string(&json_writer, AZ_SPAN_FROM_BUFFER(component_specifier_value)));
-  AZ_RETURN_IF_FAILED(az_json_writer_append_property_name(
-      &json_writer, name));
-  AZ_RETURN_IF_FAILED(
-      az_json_writer_append_string(&json_writer, value));
-  AZ_RETURN_IF_FAILED(az_json_writer_append_end_object(&json_writer));
-  AZ_RETURN_IF_FAILED(az_json_writer_append_end_object(&json_writer));
+  bool has_component = az_span_ptr(component) != NULL;
 
-  *out_span = az_json_writer_get_json(&json_writer);
+  AZ_RETURN_IF_FAILED(az_json_writer_append_begin_object(json_writer));
+
+  if(has_component)
+  {
+    AZ_RETURN_IF_FAILED(az_json_writer_append_property_name(json_writer, component));
+    AZ_RETURN_IF_FAILED(az_json_writer_append_begin_object(json_writer));
+    AZ_RETURN_IF_FAILED(az_json_writer_append_property_name(
+        json_writer, AZ_SPAN_FROM_BUFFER(component_specifier_name)));
+    AZ_RETURN_IF_FAILED(
+        az_json_writer_append_string(json_writer, AZ_SPAN_FROM_BUFFER(component_specifier_value)));
+  }
+
+  AZ_RETURN_IF_FAILED(az_json_writer_append_property_name(json_writer, name));
+
+  if (*(char*)az_span_ptr(value) == '"')
+  {
+    az_span stripped_val;
+    strip_quotes_from_span(value, &stripped_val);
+    AZ_RETURN_IF_FAILED(az_json_writer_append_string(json_writer, stripped_val));
+  }
+  else
+  {
+    double value_as_double;
+    AZ_RETURN_IF_FAILED(az_span_atod(value, &value_as_double));
+    AZ_RETURN_IF_FAILED(
+        az_json_writer_append_double(json_writer, value_as_double, JSON_DOUBLE_DIGITS));
+  }
+
+  if(has_component)
+  {
+    AZ_RETURN_IF_FAILED(az_json_writer_append_end_object(json_writer));
+  }
+
+  AZ_RETURN_IF_FAILED(az_json_writer_append_end_object(json_writer));
+
+  *out_span = az_json_writer_get_json(json_writer);
 
   return AZ_OK;
+}
+
+az_result pnp_helper_get_telemetry_topic(
+    az_iot_hub_client const* client,
+    az_iot_hub_client_properties* properties,
+    az_span component_name,
+    char* mqtt_topic,
+    size_t mqtt_topic_size,
+    size_t* out_mqtt_topic_length)
+{
+  az_result result;
+
+  if ((result = az_iot_hub_client_properties_append(
+           properties, component_telemetry_prop_span, component_name))
+      != AZ_OK)
+  {
+    return result;
+  }
+  else
+  {
+    result = az_iot_hub_client_telemetry_get_publish_topic(
+        client, properties, mqtt_topic, mqtt_topic_size, out_mqtt_topic_length);
+  }
+
+  return result;
 }
 
 az_result pnp_helper_parse_command_name(
     char* component_command,
     int32_t component_command_size,
     az_span* component_name,
-    az_span* pnp_command_name,
-    az_span* out_span)
+    az_span* pnp_command_name)
 {
   az_span component_command_span
       = az_span_init((uint8_t*)component_command, component_command_size);
@@ -64,25 +168,45 @@ az_result pnp_helper_parse_command_name(
   return AZ_OK;
 }
 
-az_result pnp_helper_create_reported_property_double(
+az_result pnp_helper_create_reported_property(
     az_span json_buffer,
     az_span component_name,
     az_span property_name,
-    double property_value_double,
+    az_span property_json_value,
     az_span* out_span)
 {
   az_result result;
 
-  // Temporary code section
-  (void)property_value_double;
-  char double_as_span_buffer[16];
-  az_span double_as_span = az_span_init((uint8_t*)double_as_span_buffer, sizeof(double_as_span));
-  result = az_span_dtoa(double_as_span, property_value_double, 2, &double_as_span);
-
-  result = build_component_reported_property(
-      json_buffer, component_name, property_name, double_as_span, &json_buffer);
-
-  *out_span = json_buffer;
+  az_json_writer json_writer;
+  result = az_json_writer_init(&json_writer, json_buffer, NULL);
+  result = build_reported_property(
+      &json_writer, component_name, property_name, property_json_value, out_span);
 
   return result;
 }
+
+az_result pnp_helper_create_reported_property_with_status(
+    az_span json_buffer,
+    az_span component_name,
+    az_span property_name,
+    az_span property_json_value,
+    int32_t ack_value,
+    int32_t ack_version,
+    az_span ack_description)
+{
+  az_result result;
+
+  az_json_writer json_writer;
+  result = az_json_writer_init(&json_writer, json_buffer, NULL);
+  if (az_span_ptr(component_name) != NULL)
+  {
+    result = AZ_OK;
+  }
+  else
+  {
+    result = build_reported_property_with_status(
+        &json_writer, property_name, property_json_value, ack_value, ack_version, ack_description);
+  }
+  return result;
+}
+
--- sdk/samples/iot/hub/src/pnp_helper.h
+++ sdk/samples/iot/hub/src/pnp_helper.h
@@ -7,6 +7,37 @@
 #include <azure/core/az_json.h>
 #include <azure/core/az_result.h>
 #include <azure/core/az_span.h>
+#include <azure/iot/az_iot_hub_client.h>
+
+#define PNP_STATUS_SUCCESS 200
+#define PNP_STATUS_BAD_FORMAT 400
+#define PNP_STATUS_NOT_FOUND  404
+#define PNP_STATUS_INTERNAL_ERROR 500
+
+/**
+ * @brief Gets the MQTT topic that must be used for device to cloud telemetry messages.
+ * @remark Telemetry MQTT Publish messages must have QoS At least once (1).
+ * @remark This topic can also be used to set the MQTT Will message in the Connect message.
+ *
+ * @param[in] client The #az_iot_hub_client to use for this call.
+ * @param[in] properties An optional #az_iot_hub_client_properties object (can be NULL).
+ * @param[in] component_name An optional component name if the telemetry is being sent from a
+ *                           sub-component.
+ * @param[out] mqtt_topic A buffer with sufficient capacity to hold the MQTT topic. If
+ *                        successful, contains a null-terminated string with the topic that
+ *                        needs to be passed to the MQTT client.
+ * @param[in] mqtt_topic_size The size, in bytes of \p mqtt_topic.
+ * @param[out] out_mqtt_topic_length __[nullable]__ Contains the string length, in bytes, of
+ *                                                  \p mqtt_topic. Can be `NULL`.
+ * @return #az_result
+ */
+az_result pnp_helper_get_telemetry_topic(
+    az_iot_hub_client const* client,
+    az_iot_hub_client_properties* properties,
+    az_span component_name,
+    char* mqtt_topic,
+    size_t mqtt_topic_size,
+    size_t* out_mqtt_topic_length);
 
 /**
  * @brief Parse a JSON payload for a PnP component command
@@ -21,34 +52,41 @@ az_result pnp_helper_parse_command_name(
     az_span* pnp_command_name);
 
 /**
-* @brief Build a reported property of type double
-*
-* @param[in] json_buffer The span into which the json payload will be placed.
-* @param[in] component_name The name of the component for the reported_property.
-* @param[in] property_name The name of the property to which to send an update.
-* @param[in] property_value_double The value of the property as a double.
-* @param[out] out_span The #az_span pointer to the output json payload.
-*/
-az_result pnp_helper_create_reported_property_double(
+ * @brief Build a reported property
+ *
+ * @param[in] json_buffer The span into which the json payload will be placed.
+ * @param[in] component_name The name of the component for the reported_property.
+ * @param[in] property_name The name of the property to which to send an update.
+ * @param[in] property_json_value The value of the property as valid JSON.
+ * @param[out] out_span The #az_span pointer to the output json payload.
+ */
+az_result pnp_helper_create_reported_property(
     az_span json_buffer,
     az_span component_name,
     az_span property_name,
-    double property_value_double);
+    az_span property_json_value,
+    az_span* out_span);
 
 /**
-* @brief Build a reported property of type double
-*
-* @param[in] json_buffer The span into which the json payload will be placed.
-* @param[in] component_name The name of the component for the reported_property.
-* @param[in] property_name The name of the property to which to send an update.
-* @param[in] property_value_string The value of the property as a string.
-* @param[out] out_span The #az_span pointer to the output json payload.
-*/
-az_result pnp_helper_create_reported_property_string(
+ * @brief Build a reported property
+ *
+ * @param[in] json_buffer The span into which the json payload will be placed.
+ * @param[in] component_name The name of the component for the reported_property.
+ * @param[in] property_name The name of the property to which to send an update.
+ * @param[in] property_json_value The value of the property as valid JSON.
+ * @param[in] ack_value The return value for the reported property.
+ * @param[in] ack_version The ack version for the reported property.
+ * @param[in] ack_description The optional description for the reported property.
+ * @param[out] out_span The #az_span pointer to the output json payload.
+ */
+az_result pnp_helper_create_reported_property_with_status(
     az_span json_buffer,
     az_span component_name,
     az_span property_name,
-    az_span property_value_string);
+    az_span property_json_value,
+    int32_t ack_value,
+    int32_t ack_version,
+    az_span ack_description);
 
-// az_result pnp_helper_
+
 #endif // _az_PNP_HELPER_H
