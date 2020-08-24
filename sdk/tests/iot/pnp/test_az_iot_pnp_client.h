b8162d370476c0409aea28ffbb90ee29cf917ed9
Dane Walton <dane.walton@microsoft.com>
2020-08-24 10:41:36 -0700

    wip

---
+++ sdk/tests/iot/pnp/test_az_iot_pnp_client.h
@@ -0,0 +1,8 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+int test_iot_pnp_client();
+int test_iot_pnp_client_telemetry();
+int test_iot_pnp_client_twin();
+int test_iot_pnp_client_methods();
+int test_iot_sas_token();
