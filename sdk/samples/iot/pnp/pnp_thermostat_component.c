c63a53845f5d36b650d0389c5bc2552985df76e3
Dane Walton <dane.walton@microsoft.com>
2020-09-18 09:13:06 -0700

    update sample

--- sdk/samples/iot/pnp/pnp_thermostat_component.c
+++ sdk/samples/iot/pnp/pnp_thermostat_component.c
@@ -260,19 +260,25 @@ az_result pnp_thermostat_process_property_update(
     az_json_writer jw;
     az_result rc = az_json_writer_init(&jw, payload, NULL);
 
-    rc = az_json_writer_append_begin_object(&jw);
-    rc = az_iot_pnp_client_twin_begin_property_with_status(
-        pnp_client,
-        &jw,
-        ref_thermostat_component->component_name,
-        property_name->slice,
-        (int32_t)AZ_IOT_STATUS_OK,
-        version,
-        twin_response_success);
-    rc = az_json_writer_append_double(&jw, parsed_property_value, DOUBLE_DECIMAL_PLACE_DIGITS);
-    rc = az_iot_pnp_client_twin_end_property_with_status(
-        pnp_client, &jw, ref_thermostat_component->component_name);
-    rc = az_json_writer_append_end_object(&jw);
+    if ((rc = az_json_writer_append_begin_object(&jw))
+        || (rc = az_iot_pnp_client_twin_begin_property_with_status(
+                pnp_client,
+                &jw,
+                ref_thermostat_component->component_name,
+                property_name->slice,
+                (int32_t)AZ_IOT_STATUS_OK,
+                version,
+                twin_response_success))
+        || (rc
+            = az_json_writer_append_double(&jw, parsed_property_value, DOUBLE_DECIMAL_PLACE_DIGITS))
+        || (rc = az_iot_pnp_client_twin_end_property_with_status(
+                pnp_client, &jw, ref_thermostat_component->component_name))
+        || (rc = az_json_writer_append_end_object(&jw)))
+    {
+      IOT_SAMPLE_LOG_ERROR(
+          "Failed to create property with status payload, az_result return code 0x%08x.", rc);
+      exit(rc);
+    }
 
     if (az_result_failed(rc))
     {
